41daffb7ca5f8a76bd1a904b666c9082e8864298
Sungkuk Park <sungkuk.park@wooga.net>
2025-03-14 17:34:55 +0100

    Add hello world program

---
+++ hello.c
@@ -0,0 +1,12 @@
+// 표준 라이브러리에 대한 정보를 담고 있다.
+#include <stdio.h>
+
+// "main"이라는 이름의 함수를 정의하고
+// 그 어떤 인자 값(argument value)도 받지 않는다.
+void main()
+{
+    // "main" 함수는 라이브러리 함수 "printf"를 호출하며,
+    // printf 함수는 이 문자열(sequence of characters)을 출력한다.
+    // "\n"은 개행(newline) 문자를 의미한다.
+    printf("hello, world\n");
+}
