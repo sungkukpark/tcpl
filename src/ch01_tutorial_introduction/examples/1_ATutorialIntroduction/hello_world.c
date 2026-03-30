4001148ec8ee5941c6e3b42963a6e7c3735dd761
Sungkuk Park <sungkukshawnpark@gmail.com>
2026-03-30 20:57:16 +0200

    add more comments

--- src/ch01_tutorial_introduction/examples/1_ATutorialIntroduction/hello_world.c
+++ src/ch01_tutorial_introduction/examples/1_ATutorialIntroduction/hello_world.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>				// include information about standard library
 
-int main(int argc, char argv[])
+int main(int argc, char argv[])	// define a function named main
+								// that receives no argument values
 {
 	printf("hello, world\n");
 }
